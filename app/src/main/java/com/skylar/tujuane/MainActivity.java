5edfaca45fcf18f1ade230f1d0e28d48c01ca662
Lorenah <lorayne911@gmail.com>
2021-01-18 18:50:42 +0300

    implement  butterknife

--- app/src/main/java/com/skylar/tujuane/MainActivity.java
+++ app/src/main/java/com/skylar/tujuane/MainActivity.java
@@ -3,8 +3,19 @@ package com.skylar.tujuane;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.appNameTextView)
+    TextView mAppNameTextView;
+    @BindView(R.id.locationEditText)
+    EditText mLocationEditText;
+    @BindView(R.id.findActivityButton)
+    Button findActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
