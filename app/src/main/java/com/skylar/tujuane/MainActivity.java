c2f90f0ad6e72478de2cf19138c5fd5061d775e5
Lorenah <lorayne911@gmail.com>
2021-01-18 19:38:05 +0300

    add onclick listener interface

--- app/src/main/java/com/skylar/tujuane/MainActivity.java
+++ app/src/main/java/com/skylar/tujuane/MainActivity.java
@@ -15,22 +15,23 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
     @BindView(R.id.appNameTextView)
     TextView mAppNameTextView;
-    @BindView(R.id.locationEditText)
-    EditText mLocationEditText;
-    @BindView(R.id.findActivityButton)
-    Button mFindActivityButton;
+
+    private EditText mLocationEditText;
+    private Button mFindActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        mFindActivityButton = (Button) findViewById(R.id.findActivityButton);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindActivityButton.setOnClickListener(this);
     }
 
 
-    public void onClick(View face){
-        if(face == mFindActivityButton) {
+    public void onClick(View face) {
+        if (face == mFindActivityButton) {
             String location = mLocationEditText.getText().toString();
             Intent intent = new Intent(MainActivity.this, secondaryActivity.class);
             intent.putExtra("location", location);
