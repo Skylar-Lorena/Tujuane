34593483e7a8219dad73be1ceea85c1889d33d94
Lorenah <lorayne911@gmail.com>
2021-01-18 19:08:37 +0300

    add and implement intent to swith activity

--- app/src/main/java/com/skylar/tujuane/MainActivity.java
+++ app/src/main/java/com/skylar/tujuane/MainActivity.java
@@ -2,12 +2,15 @@ package com.skylar.tujuane;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.appNameTextView)
@@ -15,11 +18,22 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.locationEditText)
     EditText mLocationEditText;
     @BindView(R.id.findActivityButton)
-    Button findActivityButton;
+    Button mFindActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mFindActivityButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, secondaryActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
     }
 }
